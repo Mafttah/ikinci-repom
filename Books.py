4c5afe3b46f246815a156d928c116ab1d4b1e74d
Bora Saglam <posarobit@gmail.com>
2025-06-07 12:55:40 +0300

    Books.py eklemeler yaptım.

--- Books.py
+++ Books.py
@@ -1,5 +1,148 @@
-print("Percy Jackson and Olympians")
-print("Percy Jackson and Olympos Heroes")
-print("Cane Chronikles")
-print("Magnus Chase and Gods of Asgard")
-print("Trialls of Apollo")
+import keyboard
+print("Books")
+print("")
+print("Devam etmek için spacebar'a basiniz.")
+keyboard.wait("space")
+
+print("Rick Riordan's Books")
+print("")
+
+print("Devam etmek için enter'a basiniz.")
+keyboard.wait("enter")
+
+print("Percy Jackson and Olympians Series")
+
+print("Devam etmek için spacebar'a basiniz.")
+keyboard.wait("space")
+
+print("1. Lightning Thief")
+print("2. Sea of Monsters")
+print("3. Titan’s Curse")
+print("4. Battle of the Labyrinth")
+print("5. Last Olympian")
+print("")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("Percy Jackson and Olympos Heroes Series")
+print("")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("1. Lost Hero")
+print("2. Son of Neptune")
+print("3. Mark of Athena")
+print("4. House of Hades")
+print("5.Blood of Olympus")
+print("")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("Kane Chronicles Series")
+print("")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("1. Red Pyramid")
+print("2. Throne of Fire")
+print("3. Serpent’s Shadow")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("Magnus Chase and Gods of Asgard Series")
+print("")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("1. Sword of Summer")
+print("2. Hammer of Thor")
+print("Ship of the Dead")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("Trialls of Apollo Series")
+print("")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("1. Hidden Oracle")
+print("2. Dark Prophecy")
+print("3. Burning Maze")
+print("4. Tyrant’s Tomb")
+print("5. Tower of Nero")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("John Flanagan's Books")
+print("")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("1. Ranger's Apprentice Books Series")
+print("")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("Early years book 1. The Tournament at Gorlan")
+print("Early years book 2. The Battle of Hackham Heath")
+print("1. The Ruins of Gorlan")
+print("2. The Burning Bridge")
+print("3. The Icebound Land")
+print("4. The Battle for Skandia")
+print("5. The Sorcerer in the North")
+print("6. The Siege of Macindaw")
+print("7. Erak's Ransom")
+print("8. The Kings of Clonmel")
+print("9. Halt's Peril")
+print("10. The Emperor of Nihon-Ja")
+print("11. The Lost Stories")
+print("")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("2. The Royal Ranger Series")
+
+print("12. The Royal Ranger")
+print("13. The Red Fox Clan")
+print("14. Duel at Araluen")
+print("15. The Missing Prince")
+print("16. Escape from Falaise")
+print("17. Arazan's Wolves")
+print("18. Ambush at Sorato")
+
+print("Devam etmek için spacebar'a basiniz")
+keyboard.wait("space") 
+
+print("3. Brotherband Chronicles")
+print("")
+
+print("Devam etmek için enter'a basiniz")
+keyboard.wait("enter") 
+
+print("1. The Outcasts")
+print("2. The Invaders")
+print("3. The Hunters")
+print("(4. Slaves of Socorro")
+print("5. Scorpion Mountain")
+print("6. The Ghostfaces")
+print("7. The Caldera")
+print("8. Return of the Temujai")
+print("9. The Stern Chase")
+
+print("Programdan çıkmak için Esc' ye basınız")
+
+keyboard.wait("esc")
+
+print("Programdan çıkıldı.")
