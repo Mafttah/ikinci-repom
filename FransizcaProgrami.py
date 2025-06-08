1293372fe274aa37029086b6a79f61ba2e6f062d
Bora Saglam <posarobit@gmail.com>
2025-06-08 12:47:14 +0300

    FransizcaProgrami.py ve ItalyancaProgrami.py değişiklik yaptım.

--- FransizcaProgrami.py
+++ FransizcaProgrami.py
@@ -1,7 +1,22 @@
+import keyboard
 print("SJ'apprends le français = I am learning French")
+print("")
+
+print("Greeting")
+
+print("Devam etmek için spacebar'a basiniz.")
+keyboard.wait("space")
+print("")
+
 print("Salut = Hello")
 print("Comment ca va = How are you?")
 print("Encante = Nice to meet you")
 print("A_bientot = See you soon")
 print("Bonne_soiree = Good Evening")
 print("A_demain = See you tomorrow")
+
+print("Programdan çıkmak için Esc' ye basınız")
+
+keyboard.wait("esc")
+
+print("Programdan çıkıldı.")
--- ItalyancaProgrami.py
+++ ItalyancaProgrami.py
@@ -1,4 +1,12 @@
+import keyboard
 print("Sto imparando l'italiano = I am learning Italian")
+
+print("Greeting")
+
+print("Devam etmek için spacebar'a basiniz.")
+keyboard.wait("space")
+print("")
+
 print("Salve = Hello")
 print("Come stai = How are you?")
 print("Piacere = Nice to meet you")
@@ -6,3 +14,9 @@ print("A_presto = See you soon")
 print("Buona_Sera = Good Evening")
 print("Arrivederci = Good bye")
 print("Di dove sei = Where are you from")
+
+print("Programdan çıkmak için Esc' ye basınız")
+
+keyboard.wait("esc")
+
+print("Programdan çıkıldı.")
